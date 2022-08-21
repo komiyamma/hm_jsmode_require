6789ee460c25cba17ba27f5fe690bd23c0bb6427
komiyamma <komiyama0629@gmail.com>
2022-08-22 08:31:18 +0900

    定義ファイルをちゃんと記載

--- types/require.d.ts
+++ types/require.d.ts
@@ -10,17 +10,37 @@
  * require関数は、node-js の require の超簡易版です。    
  * JSONファイル、もしくは、JavaScriptファイルを読み込むことが出来ます。    
  *
- * - currentmacrodirectory (現在実行しているマクロのディレクトリ)
- * - macrodir/jsmode_modules (指定のマクロ用フォルダのjsmode_modulesというサブディレクトリ)
- * - hidemarudir/jsmode_modules (秀丸本体ディレクトリのjsmode_modulesというサブディレクトリ)
- * - カレントフォルダ
+ * @param module_path
+ * モジュールのパスを指定します。
+ * 
+ * - 検索順序
+ *   1. currentmacrodirectory/[module_path(.js)]
+ *     (現在実行しているマクロのディレクトリ)    
+ *   2. macrodir/jsmode_modules/[module_path(.js)]
+ *     (指定のマクロ用フォルダのjsmode_modulesというサブディレクトリ)    
+ *   3. macrodir/jsmode_modules/[module_path]/[module_path(.js)]
+ *     (指定のマクロ用フォルダのjsmode_modulesというサブディレクトリ下の指定の文字列と同じサブディレクトリ)    
+ *   4. hidemarudir/jsmode_modules/[module_path(.js)]    
+ *     (秀丸本体ディレクトリのjsmode_modulesというサブディレクトリ)    
+ *   5. hidemarudir/jsmode_modules/[module_path]/[module_path(.js)]    
+ *     (秀丸本体ディレクトリのjsmode_modulesというサブディレクトリ下の指定の文字列と同じサブディレクトリ)    
+ *   6. カレントフォルダ    
  *
  * の順番にファイルを探します。
  * 
- * - module.exports
- * - exports 
+ * - module.exports    
+ * - exports     
+ * 
  * のみが実装されています。    
- * 他の機能やプロパティはありません。    
- * キャッシュ機能はありません。
+ * 
+ * - nodeのrequireにあるような、他の様々な機能やプロパティはありません。    
+ * - nodeのrequireにあるような、キャッシュ機能はありません。
+ * 
+ * @see jsmode の require 関数
+ * @link https://xn--pckzexbx21r8q9b.net/?page=nobu_tool_hm_jsmode_require
+ *
+ * @returns
+ * 指定のモジュールのオブジェクト。    
+ * 読み込みに失敗した場合は例外が発生する。    
  */
 declare function require(module_path: string): any;
